36680df09706cacaff5f7e971ed761f47f8364e4
sandeepnayak-wal <sandeep.megavath@veltris.com>
2025-04-01 21:35:16 +0530

    Initial Commit

---
+++ haystekEcomLiteTests/haystekEcomLiteTests.swift
@@ -0,0 +1,17 @@
+//
+//  haystekEcomLiteTests.swift
+//  haystekEcomLiteTests
+//
+//  Created by Sandeep on 01/04/25.
+//
+
+import Testing
+@testable import haystekEcomLite
+
+struct haystekEcomLiteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
